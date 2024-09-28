c3046b959b2b0734df876a552a42f5cac95e9e43
md-mannan <mdmannan580@gmail.com>
2024-09-28 23:47:14 +0300

    finished untill 1"42 minutes

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,4 +1,5 @@
 import { usePage } from "@inertiajs/react";
+import { useEffect } from "react";
 const ChatLayout = ({ user, header, children }) => {
     const page = usePage();
     const conversations = page.props.conversations;
@@ -6,6 +7,28 @@ const ChatLayout = ({ user, header, children }) => {
 
     console.log("Conversations", conversations);
     console.log("selectedConversation", selectedConversation);
+
+    useEffect(() => {
+        Echo.join("online")
+            .here((users) => {
+                console.log("Users here:", users);
+            })
+            .joining((user) => {
+                console.log("User joining:", user);
+            })
+            .leaving((user) => {
+                console.log("User Leaving:", user);
+            })
+
+            .error((error) => {
+                console.error("Echo Error:", error);
+            });
+
+        return () => {
+            Echo.leave("online");
+        };
+    }, []);
+
     return (
         <>
             ChatLayout

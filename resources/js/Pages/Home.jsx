d2918c3754eaf5a11c3456ba275a1d61b9ec7351
md-mannan <mdmannan580@gmail.com>
2024-09-28 22:25:19 +0300

    DB Factory Generated and Route Configured

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,7 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
 
-export default function Dashboard() {
+export default function Home(auth) {
     return (
         <AuthenticatedLayout
             header={
